8aa9c01975afd82d4aa5f8497942d62ed4776b9d
Sneha Mahajan <sneh.km@gmail.com>
2022-10-17 23:29:24 -0400

    Updates

---
+++ precache-manifest.9ca0c1a1f1a2114319e47b64573d77ba.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "1e26880c6cb987893962",
+    "url": "/werun/static/css/main.045c8208.chunk.css"
+  },
+  {
+    "revision": "1e26880c6cb987893962",
+    "url": "/werun/static/js/main.fcbb18db.chunk.js"
+  },
+  {
+    "revision": "7f5f1b2757133e877456",
+    "url": "/werun/static/js/runtime~main.8fc729ff.js"
+  },
+  {
+    "revision": "f36200da0cf054bf72e6",
+    "url": "/werun/static/css/2.d4ac2036.chunk.css"
+  },
+  {
+    "revision": "f36200da0cf054bf72e6",
+    "url": "/werun/static/js/2.1bf4a26a.chunk.js"
+  },
+  {
+    "revision": "b68a6d7108db3198fb32ed1dab489156",
+    "url": "/werun/static/media/main-logo.b68a6d71.png"
+  },
+  {
+    "revision": "a34ad9fa9bda1889275feb583dd75061",
+    "url": "/werun/static/media/profile-photo.a34ad9fa.jpeg"
+  },
+  {
+    "revision": "e97d4e0a9a1f4c9958a3dc08b55d097c",
+    "url": "/werun/static/media/large-logo.e97d4e0a.svg"
+  },
+  {
+    "revision": "f1dc6b3a4c0e4ac2da952a539bc494a3",
+    "url": "/werun/static/media/rectangle-logo.f1dc6b3a.svg"
+  },
+  {
+    "revision": "6c9a529741eafb4a852116a2882e65ac",
+    "url": "/werun/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/werun/precache-manifest.9ca0c1a1f1a2114319e47b64573d77ba.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/werun/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
