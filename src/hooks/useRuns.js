2d4e6e03a7744bcbed967355a6de47fc9973447e
Sneha Mahajan <sneh.km@gmail.com>
2022-10-30 18:22:02 -0400

    Separate useEffect into another hook.

---
+++ src/hooks/useRuns.js
@@ -0,0 +1,70 @@
+import { atom, useRecoilState, useSetRecoilState } from "recoil";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { recoilPersist } from "recoil-persist";
+import { userState } from "./useAppData";
+
+// Used to persist global app state after manual refreshes.
+// See key in local storage for browser
+const { persistAtom } = recoilPersist();
+
+
+export const runsState = atom({
+  key: "runsState",
+  default: null,
+  effects_UNSTABLE: [persistAtom],
+});
+
+export const runnerRunsState = atom({
+  key: "runnerRunsState",
+  default: null,
+  effects_UNSTABLE: [persistAtom],
+});
+
+export const plannerRunsState = atom({
+  key: "plannerRunsState",
+  default: null,
+  effects_UNSTABLE: [persistAtom],
+});
+
+export default function useGetUsersRuns(options) {
+  const setRuns = useSetRecoilState(runsState);
+  const [runnerRuns, setRunnerRuns] = useRecoilState(runnerRunsState);
+  const setPlannerRuns = useSetRecoilState(plannerRunsState);
+  const [user, setUser] = useRecoilState(userState);
+
+  useEffect(() => {
+    Promise.all([axios.get("https://werun-server.herokuapp.com/api/runs")])
+      .then((response) => {
+        const { runs } = response[0].data;
+        setRuns(runs);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  useEffect(() => {
+    if (user) {
+      Promise.all([
+        axios.get(
+          `https://werun-server.herokuapp.com/api/runs/runner/${user.id}`
+        ),
+        axios.get(
+          `https://werun-server.herokuapp.com/api/runs/planner/${user.id}`
+        ),
+      ])
+        .then((response) => {
+          const { runnerRuns } = response[0].data;
+          const { plannerRuns } = response[1].data;
+          setRunnerRuns(runnerRuns);
+          setPlannerRuns(plannerRuns);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  }, [user, options.timeUpdate]);
+
+  return {};
+}
